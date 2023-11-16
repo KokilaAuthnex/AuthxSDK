887f47270cb263d3d7ddc03a222422eb4820583c
Kokila Ekanayake <kokila@authnex.com>
2023-11-16 16:33:33 +0530

    SDK Updated

---
+++ AuthXSDK.framework/Headers/AuthXSDK.h
@@ -0,0 +1,18 @@
+//
+//  AuthXSDK.h
+//  AuthXSDK
+//
+//  Created by Lahiru Chathuranga on 2022-10-27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AuthXSDK.
+FOUNDATION_EXPORT double AuthXSDKVersionNumber;
+
+//! Project version string for AuthXSDK.
+FOUNDATION_EXPORT const unsigned char AuthXSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthXSDK/PublicHeader.h>
+
+
